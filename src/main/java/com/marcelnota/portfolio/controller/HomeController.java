d2df998198509f30cb697fbcc83be4c8766e12cd
MarcelNota <n3gr1t0.mn@gmail.com>
2026-08-28 20:34:26 +0200

    Initial portfolio version

---
+++ src/main/java/com/marcelnota/portfolio/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.marcelnota.portfolio.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/","", "/home"})
+    public String showHomePage(Model model){
+        model.addAttribute("title", "Home");
+        return "master";
+    }
+
+    @GetMapping({"/projects"})
+    public String showProjectsPage(Model model){
+        model.addAttribute("title", "Projects");
+        return "master";
+    }
+
+    @GetMapping({"/resume"})
+    public String showresumePage(Model model){
+        model.addAttribute("title", "Resume");
+        return "master";
+    }
+
+    @GetMapping({"/contact"})
+    public String showContactPage(Model model){
+        model.addAttribute("title", "Contact");
+        return "master";
+    }
+}
